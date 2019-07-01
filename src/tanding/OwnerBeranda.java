82d4b2014d947647827df7a5ee47190f6a0992a2
Ramadhina <Ramadhina@DESKTOP-UCE94GU>
2019-07-01 21:50:13 +0700

    koding OwnerBeranda

--- src/tanding/OwnerBeranda.java
+++ src/tanding/OwnerBeranda.java
@@ -5,6 +5,12 @@
  */
 package tanding;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Ramadhina
@@ -15,6 +21,14 @@ public class OwnerBeranda extends javax.swing.JFrame {
      * Creates new form OwnerBeranda
      */
     private String id_owner;
+    private String fieldName;
+    private String contact_number;
+    private String address;
+    
+    Connection conn = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+    
     public OwnerBeranda() {
         initComponents();
     }
@@ -32,14 +46,11 @@ public class OwnerBeranda extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        label_ownerName = new javax.swing.JLabel();
-        label_ownerNumber = new javax.swing.JLabel();
-        label_ownerAddress = new javax.swing.JLabel();
+        label_fieldName = new javax.swing.JLabel();
+        label_contactNumber = new javax.swing.JLabel();
+        label_Address = new javax.swing.JLabel();
         jDesktopPane1 = new javax.swing.JDesktopPane();
-        o_name = new javax.swing.JTextField();
-        o_contactnum = new javax.swing.JTextField();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        o_address = new javax.swing.JTextArea();
+        imagelabel = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
         jPanel6 = new javax.swing.JPanel();
@@ -47,35 +58,46 @@ public class OwnerBeranda extends javax.swing.JFrame {
         order_table = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
 
         jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 2, true));
 
         jLabel1.setFont(jLabel1.getFont().deriveFont((jLabel1.getFont().getStyle() | java.awt.Font.ITALIC) | java.awt.Font.BOLD, jLabel1.getFont().getSize()+7));
         jLabel1.setText("OWNER PROFILE");
 
-        label_ownerName.setFont(label_ownerName.getFont().deriveFont((label_ownerName.getFont().getStyle() & ~java.awt.Font.ITALIC) | java.awt.Font.BOLD, label_ownerName.getFont().getSize()+3));
-        label_ownerName.setText("Owner Name");
+        label_fieldName.setFont(label_fieldName.getFont().deriveFont((label_fieldName.getFont().getStyle() & ~java.awt.Font.ITALIC) | java.awt.Font.BOLD, label_fieldName.getFont().getSize()+3));
+        label_fieldName.setText("Field Name");
 
-        label_ownerNumber.setFont(label_ownerNumber.getFont().deriveFont(label_ownerNumber.getFont().getStyle() | java.awt.Font.BOLD, label_ownerNumber.getFont().getSize()+3));
-        label_ownerNumber.setText("Contact Number");
+        label_contactNumber.setFont(label_contactNumber.getFont().deriveFont(label_contactNumber.getFont().getStyle() | java.awt.Font.BOLD, label_contactNumber.getFont().getSize()+3));
+        label_contactNumber.setText("Contact Number");
 
-        label_ownerAddress.setFont(label_ownerAddress.getFont().deriveFont(label_ownerAddress.getFont().getStyle() | java.awt.Font.BOLD, label_ownerAddress.getFont().getSize()+3));
-        label_ownerAddress.setText("Address");
+        label_Address.setFont(label_Address.getFont().deriveFont(label_Address.getFont().getStyle() | java.awt.Font.BOLD, label_Address.getFont().getSize()+3));
+        label_Address.setText("Address");
+
+        imagelabel.setText("jLabel2");
+
+        jDesktopPane1.setLayer(imagelabel, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
         javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
         jDesktopPane1.setLayout(jDesktopPane1Layout);
         jDesktopPane1Layout.setHorizontalGroup(
             jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDesktopPane1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(imagelabel, javax.swing.GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)
+                .addContainerGap())
         );
         jDesktopPane1Layout.setVerticalGroup(
             jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 273, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDesktopPane1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(imagelabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
-
-        o_address.setColumns(20);
-        o_address.setRows(5);
-        jScrollPane2.setViewportView(o_address);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -84,23 +106,15 @@ public class OwnerBeranda extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jDesktopPane1)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(label_ownerNumber)
-                            .addComponent(label_ownerAddress)
-                            .addComponent(label_ownerName))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jScrollPane2)
-                            .addComponent(o_contactnum)
-                            .addComponent(o_name))
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addContainerGap())
+                    .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(label_contactNumber)
+                    .addComponent(label_Address)
+                    .addComponent(label_fieldName))
+                .addContainerGap(19, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(69, 69, 69))
+                .addGap(33, 33, 33))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -110,18 +124,12 @@ public class OwnerBeranda extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jDesktopPane1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(label_ownerName)
-                    .addComponent(o_name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(label_fieldName)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(label_ownerNumber)
-                    .addComponent(o_contactnum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(label_contactNumber)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(label_ownerAddress)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                .addComponent(label_Address)
+                .addGap(90, 90, 90))
         );
 
         jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
@@ -179,7 +187,7 @@ public class OwnerBeranda extends javax.swing.JFrame {
                 .addComponent(jLabel5)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, 323, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(113, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -191,7 +199,7 @@ public class OwnerBeranda extends javax.swing.JFrame {
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(19, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -201,6 +209,35 @@ public class OwnerBeranda extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        // TODO add your handling code here:
+        try{
+         String sql = "select * from fieldowner where id_field = '"+id_owner+"' ";
+         pst = conn.prepareStatement(sql);
+         
+         rs = pst.executeQuery();
+         if(rs.next()){
+             fieldName = rs.getString("fieldname");
+             contact_number = rs.getString("contact");
+             address = rs.getString("address");
+             label_fieldName.setText(fieldName);
+             label_contactNumber.setText(contact_number);
+             label_Address.setText(address);
+             
+             byte[] imagedata = rs.getBytes("photo");
+             viewimage = new ImageIcon(imagedata);
+             imagelabel.setIcon(viewimage);
+         }
+         else{
+             JOptionPane.showMessageDialog(null, "Field Name didn't show");
+            }
+        }
+        catch(Exception ex){
+        JOptionPane.showMessageDialog(null, "Error : "+ex.getMessage());
+        }
+        
+    }//GEN-LAST:event_formWindowOpened
 
     /**
      * @param args the command line arguments
@@ -238,6 +275,7 @@ public class OwnerBeranda extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel imagelabel;
     private javax.swing.JDesktopPane jDesktopPane1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel5;
@@ -245,13 +283,11 @@ public class OwnerBeranda extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JLabel label_ownerAddress;
-    private javax.swing.JLabel label_ownerName;
-    private javax.swing.JLabel label_ownerNumber;
-    private javax.swing.JTextArea o_address;
-    private javax.swing.JTextField o_contactnum;
-    private javax.swing.JTextField o_name;
+    private javax.swing.JLabel label_Address;
+    private javax.swing.JLabel label_contactNumber;
+    private javax.swing.JLabel label_fieldName;
     private javax.swing.JTable order_table;
     // End of variables declaration//GEN-END:variables
-}
+    private ImageIcon viewimage = null;
+
+}   
