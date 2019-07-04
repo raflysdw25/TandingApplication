ea776ebc1e46107a2f3c19505d0b26607d3345e2
RaflySadewa <raflysdw25@gmail.com>
2019-07-04 22:49:42 +0700

    Upload Baru : 1. Code untuk VisitField dan VisitTeam done 2. Buat konstruktor baru di FormPertandingan

--- src/application/VisitField.java
+++ src/application/VisitField.java
@@ -5,17 +5,35 @@
  */
 package application;
 
+import connection.DatabaseTanding;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Ramadhina
  */
 public class VisitField extends javax.swing.JFrame {
-
+    Connection conn;
+    PreparedStatement pst;
+    ResultSet rs;
+    
+    private String idTeam, idField;
     /**
      * Creates new form VisitField
      */
     public VisitField() {
         initComponents();
+    }
+    
+    public VisitField(String id_team, String id_field){
+        initComponents();
+        conn = DatabaseTanding.getConnection();
+        idTeam = id_team;
+        idField = id_field;
     }
 
     /**
@@ -28,13 +46,21 @@ public class VisitField extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jDesktopPane1 = new javax.swing.JDesktopPane();
         fieldimage = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        namaField = new javax.swing.JLabel();
+        alamatField = new javax.swing.JLabel();
+        kontakField = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txt_fasilitas = new javax.swing.JTextArea();
+        btn_sewa = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
 
         jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
@@ -59,14 +85,22 @@ public class VisitField extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel1.setText("Nama Lapangan");
+        namaField.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        namaField.setText("Nama Lapangan");
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel2.setText("Alamat");
+        alamatField.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        alamatField.setText("Alamat");
 
-        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel3.setText("Kontak");
+        kontakField.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        kontakField.setText("Kontak");
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel4.setText("Fasilitias");
+
+        txt_fasilitas.setEditable(false);
+        txt_fasilitas.setColumns(20);
+        txt_fasilitas.setRows(5);
+        jScrollPane1.setViewportView(txt_fasilitas);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -77,10 +111,15 @@ public class VisitField extends javax.swing.JFrame {
                 .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel3))
-                .addContainerGap(200, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(namaField)
+                            .addComponent(alamatField)
+                            .addComponent(kontakField)
+                            .addComponent(jLabel4))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 289, Short.MAX_VALUE))
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -89,17 +128,26 @@ public class VisitField extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(29, 29, 29)
-                        .addComponent(jLabel2)
-                        .addGap(63, 63, 63)
-                        .addComponent(jLabel3)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(namaField)
+                        .addGap(11, 11, 11)
+                        .addComponent(alamatField)
+                        .addGap(11, 11, 11)
+                        .addComponent(kontakField)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1)))
+                .addContainerGap())
         );
 
-        jButton1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jButton1.setText("Sewa Lapangan");
-        jButton1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        btn_sewa.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        btn_sewa.setText("Sewa Lapangan");
+        btn_sewa.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        btn_sewa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_sewaActionPerformed(evt);
+            }
+        });
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/home.png"))); // NOI18N
 
@@ -115,7 +163,7 @@ public class VisitField extends javax.swing.JFrame {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jLabel5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(btn_sewa, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -126,7 +174,7 @@ public class VisitField extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btn_sewa, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addContainerGap())
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
@@ -135,6 +183,41 @@ public class VisitField extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        // TODO add your handling code here:
+        String sql = "select * from fieldowner where id_field = ?";
+        try {
+            pst = conn.prepareStatement(sql);
+            pst.setString(1, idField);
+            rs = pst.executeQuery();
+            if(rs.next()){
+                String fieldname = rs.getString("fieldname");
+                namaField.setText(fieldname);
+                
+                String contact = rs.getString("contact");
+                kontakField.setText(contact);
+                
+                String address = rs.getString("address");
+                alamatField.setText(address);
+                
+                String facilities = rs.getString("facilities");
+                txt_fasilitas.setText(facilities);
+                
+                byte[] imagedata = rs.getBytes("photo");
+                viewimage = new ImageIcon(imagedata);
+                fieldimage.setIcon(viewimage);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }//GEN-LAST:event_formWindowOpened
+
+    private void btn_sewaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_sewaActionPerformed
+        // TODO add your handling code here:
+        new Transaksi(idTeam, idField).setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_btn_sewaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -172,13 +255,17 @@ public class VisitField extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel alamatField;
+    private javax.swing.JButton btn_sewa;
     private javax.swing.JLabel fieldimage;
-    private javax.swing.JButton jButton1;
     private javax.swing.JDesktopPane jDesktopPane1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel kontakField;
+    private javax.swing.JLabel namaField;
+    private javax.swing.JTextArea txt_fasilitas;
     // End of variables declaration//GEN-END:variables
+    private ImageIcon viewimage = null;
 }
